15942020863f9849be9c93df93ba2ddf808dc7d5
ushden <ushden48@gmail.com>
2020-03-01 14:27:00 +0200

    Header done :)

--- js/main.js
+++ js/main.js
@@ -2,3 +2,21 @@ $('.burger').click(function(){
   $('.burger-item').toggleClass('burger-item_open');
   $('.nav__list').toggleClass('nav__list-open');
 });
+
+$(window).scroll(function(){
+  if ($(this).scrollTop()>=200) {
+    $('.logo').addClass('logo-scroll');
+  }
+  else {
+    $('.logo').removeClass('logo-scroll');
+  }
+});
+
+$(window).scroll(function(){
+  if ($(this).scrollTop()>=200) {
+    $('.burger').addClass('burger-scroll');
+  }
+  else {
+    $('.burger').removeClass('burger-scroll');
+  }
+});
